9ab0931f81a638150e0a8c4fc5ef9528a992dc4e
Sebastian Slangerup <sebastian.slangerup1@gmail.com>
2023-11-01 08:16:39 +0100

    Added caching to try and speed up index method

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers;
 use App\Http\Requests\DataRequest;
 use App\Models\Data;
 use App\Models\Sensor;
+use Illuminate\Support\Facades\Cache;
 
 class DataController extends Controller
 {
     public function index()
     {
-        return Data::with(['sensor', 'sensor.location', 'sensor.dataType'])->get();
+        Cache::remember('data', 60, function () {
+            return Data::with(['sensor', 'sensor.location', 'sensor.dataType'])->get();
+        });
     }
 
     public function create(DataRequest $request)

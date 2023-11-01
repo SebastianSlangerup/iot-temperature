587257c3479cd752746d3d3d64fe226e60974d27
Sebastian Slangerup <sebastian.slangerup1@gmail.com>
2023-11-01 10:11:46 +0100

    Testing

--- app/Http/Requests/DataRequest.php
+++ app/Http/Requests/DataRequest.php
@@ -22,7 +22,7 @@ class DataRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'value' => 'required|decimal:1,4|numeric',
+            'value' => 'required|decimal:0,4|numeric',
             'sensor_id' => 'required|integer|numeric'
         ];
     }
